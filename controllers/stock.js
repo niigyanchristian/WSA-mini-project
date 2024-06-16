c4155f52a8e12131a74c11e7a29de19b16bba3e5
niigyanchristian <niigyanchristian@gmail.com>
2024-06-16 09:15:21 +0000

    code cleaning

--- controllers/stock.js
+++ controllers/stock.js
@@ -4,6 +4,6 @@ exports.getStock = async(req,res)=>{
     const id = req.params.id;
 
     // const data =  await pool.query(`SELECT * FROM Products WHERE id = ${id};`)
-    const data = await Product.findById(id);
+    const data = await Product.findById(id,{stock_quantity:1});
     res.status(200).json(data);
 }
