9b69506092a707a8da4adfc2a251e870b1f9db7c
niigyanchristian <niigyanchristian@gmail.com>
2024-07-14 10:35:13 +0000

    checking

--- controllers/stock.js
+++ controllers/stock.js
@@ -3,7 +3,6 @@ const Product = require("../models/product ");
 exports.getStock = async(req,res)=>{
     const id = req.params.id;
 
-    // const data =  await pool.query(`SELECT * FROM Products WHERE id = ${id};`)
     const data = await Product.findById(id,{stock_quantity:1});
     res.status(200).json(data);
 }
